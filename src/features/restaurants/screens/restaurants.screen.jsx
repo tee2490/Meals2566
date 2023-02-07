9f533390043e6eb5fb0c7cd1b2b3002b11e062d2
tee2490@hotmail.com <tee2490@hotmail.com>
2023-02-07 13:40:24 +0700

    Favourites Bar

--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import {FlatList,TouchableOpacity } from "react-native";
+import React, { useContext, useState } from "react";
+import { FlatList, TouchableOpacity } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -7,6 +7,8 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { ActivityIndicator, MD2Colors } from "react-native-paper";
 import { Search } from "../components/search.component";
+import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
+import { FavouritesContext } from "../../../services/favourites/favourites.context";
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
@@ -24,36 +26,47 @@ const LoadingContainer = styled.View`
   left: 50%;
 `;
 
-export const RestaurantsScreen = ({navigation}) => {
-
+export const RestaurantsScreen = ({ navigation }) => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  console.log(error);
-
+  const [isToggled, setIsToggled] = useState(false);
+  const { favourites } = useContext(FavouritesContext);
+  //console.log(error);
 
   return (
     <SafeArea>
-            {isLoading && (
+      {isLoading && (
         <LoadingContainer>
           <Loading size={50} animating={true} color={MD2Colors.purple900} />
         </LoadingContainer>
       )}
-      <Search/>
+      <Search
+        isFavouritesToggled={isToggled}
+        onFavouritesToggle={() => setIsToggled(!isToggled)}
+      />
+      {isToggled && (
+        <FavouritesBar
+          favourites={favourites}
+          onNavigate={navigation.navigate}
+        />
+      )}
       <RestaurantList
-         data={restaurants}
-         renderItem={({ item }) => {
-           return (
-             <TouchableOpacity onPress={()=>navigation.navigate("RestaurantDetail",{restaurant:item})}>
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("RestaurantDetail", { restaurant: item })
+              }
+            >
               <Spacer position="bottom" size="large">
-               <RestaurantInfoCard restaurant={item} />
-             </Spacer>
-             </TouchableOpacity>
-           );
-         }}
-        keyExtractor={(item,index) => index}
+                <RestaurantInfoCard restaurant={item} />
+              </Spacer>
+            </TouchableOpacity>
+          );
+        }}
+        keyExtractor={(item, index) => index}
         contentContainerStyle={{ padding: 16 }}
       />
     </SafeArea>
   );
 };
-
-
